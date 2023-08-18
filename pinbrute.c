fb35308848cb7180b846e3077ddc3567c9d16975
mdenials <46415607+mdenials@users.noreply.github.com>
2023-08-18 15:20:23 +0700

    Update pinbrute.c

--- pinbrute.c
+++ pinbrute.c
@@ -19,7 +19,7 @@ if (strcmp((const char*) argv[i], "-b") == 0) {
 }
 }
 if (b_flag == 0 && argc > 1 && strcmp(argv[1], "-v") == 0) {
-printf("Crack 4 digits androif pin code.\n");
+printf("Crack 4 digits android pin code.\n");
 }
 if (b_flag == 1) {
   for (i= start; i <= end; i++) {
